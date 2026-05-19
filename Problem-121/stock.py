8005014398c657f85fd5b2388caf96c0941f7fae
Abi Athmakuri <aathmakuri034@gmail.com>
2026-05-19 11:09:06 -0500

    Python impl - Space Comp: O(1) -- Time Comp: O(n)

---
+++ Problem-121/stock.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        buyPrice = prices[0]
+        profit = 0
+
+        for i in range(len(prices)):
+            if (buyPrice < prices[i]):
+                if profit < prices[i] - buyPrice:
+                    profit = prices[i] - buyPrice
+            else:
+                buyPrice = prices[i]
+        
+        return profit
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    caseOne = [7,1,5,3,6,4]
+    caseTwo = [7,6,4,3,1]
+
+    print("Test case 1")
+    print("Actual Output: " + str(solution.maxProfit(caseOne)))
+    print("Expected Output: 5")
+    print("="*30)
+
+    print("Test case 2")
+    print("Actual Output: " + str(solution.maxProfit(caseTwo)))
+    print("Expected Output: 0")
+    print("="*30)
